bc681e77f2bfe41314a6ff29359302c3ba2abb40
AndreLuizMag <andreluizmagjr@outlook.com>
2021-01-07 17:49:19 -0300

    Page example

---
+++ page-home.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: ChildHome
+*/
+get_header('header');
+?>
+
+<h1>Divi Child Theme Page</h1>
+
+<span>If you use this model, you will lose support for the divi builder</span>
+
+<?php
+get_footer('footer');
+?>
